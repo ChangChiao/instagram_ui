90a030ebcca0da24a838d38f44619665545b8d4e
joe <joe.chang1014@gmail.com>
2022-03-19 13:58:37 +0800

    feat IGPost

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,19 @@
 import IGHeader from "components/IGHeader";
+import IGContainer from "components/IGContainer";
 const Home: React.FC = () => {
   return <>
     <IGHeader />
+    <IGContainer>
+      <div className="flex lg:justify-center">
+          <div className="w-full lg:w-[600px]">
+            {/* <IGStory />
+            <IGPostList /> */}
+          </div>
+          <div className="hidden lg:block lg:w-[424px]">
+            {/* <IGProfile /> */}
+          </div>
+        </div>
+    </IGContainer>
   </>;
 };
 
